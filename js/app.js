6da3e2d8b1345be6d8bf11aca9cbe1916b08dfa0
RoxanaCS <roxale.cares@gmail.com>
2018-03-04 22:37:53 -0300

    maquetado skills, contacto

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
   $('.skills').hide();
   $('.work').hide();
   $('.contact').hide();
-  setInterval('cambiar()', 3000);
 });
+var change = setInterval(
+  'cambiar()', 3000);
+
 $('.carousel').carousel({
   interval: 3000
 });
@@ -17,9 +19,9 @@ function cambiar() {
     header.css('border-color', '#FEC301');
     numero = 1;
   } else if (numero === 1) {
-    document.body.style.backgroundColor = '#CCCCCC';
-    header.css('backgroundColor', '#CCCCCC');
-    header.css('border-color', '#CCCCCC');
+    document.body.style.backgroundColor = '#023';
+    header.css('backgroundColor', '#023');
+    header.css('border-color', '#023');
     numero = 2;
   } else if (numero === 2) {
     document.body.style.backgroundColor = '#03C5C5';
@@ -38,27 +40,81 @@ function cambiar() {
     numero = 0;
   }
 }
+
+// ocultar secciones al hacer click en el menú
 $('#aboutMe').on('click', function(e) {
   e.preventDefault();
+  clearInterval(change);
+  document.body.style.backgroundColor = '#FEC301';
+  header.css('backgroundColor', '#FEC301');
+  header.css('border-color', '#FEC301');
   $('.about').show();
   $('.carousel').hide();
+  $('.skills').hide();
+  $('.work').hide();
+  $('.contact').hide();
 });
 $('#home').on('click', function(e) {
   e.preventDefault();
+  var change = setInterval(
+    'cambiar()', 3000);
   $('.about').hide();
+  $('.skills').hide();
+  $('.work').hide();
+  $('.contact').hide();
   $('.carousel').show();
 });
-var imageUrl =
+$('#skills').on('click', function(e) {
+  e.preventDefault();
+  clearInterval(change);
+  $('.skills').show();
+  $('.about').hide();
+  $('.work').hide();
+  $('.carousel').hide();
+  $('.contact').hide();
+});
 $('#work').on('click', function(e) {
   e.preventDefault();
+  clearInterval(change);
   $('.work').show();
   $('.about').hide();
+  $('.skills').hide();
   $('.carousel').hide();
   $('.contact').hide();
 });
 $('#contact').on('click', function(e) {
   e.preventDefault();
+  clearInterval(change);
   $('.about').hide();
+  $('.skills').hide();
+  $('.work').hide();
+  $('.carousel').hide();
+  $('.contact').show();
+});
+
+// botones de more info
+$('#moreAbout').on('click', function(e) {
+  e.preventDefault();
+  $('.about').show();
+  $('.carousel').hide();
+});
+$('#moreCode').on('click', function(e) {
+  e.preventDefault();
+  $('.work').show();
+  $('.carousel').hide();
+});
+$('#moreScience').on('click', function(e) {
+  e.preventDefault();
+  $('.work').show();
+  $('.carousel').hide();
+});
+$('#moreSkills').on('click', function(e) {
+  e.preventDefault();
+  $('.skills').show();
+  $('.carousel').hide();
+});
+$('#moreCont').on('click', function(e) {
+  e.preventDefault();
   $('.carousel').hide();
   $('.contact').show();
 });
